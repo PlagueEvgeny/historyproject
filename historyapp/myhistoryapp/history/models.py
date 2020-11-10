77983b50a7d6401356b702ea2729981d410f6b94
plagueevgeny <PlagueEvgeny@yandex.ru>
2020-11-10 14:17:02 +0500

    Creating two models

--- historyapp/myhistoryapp/history/models.py
+++ historyapp/myhistoryapp/history/models.py
@@ -16,3 +16,10 @@ class Personality(models.Model):
     photo = models.ImageField('Картинка', upload_to='static/img', height_field=None, width_field=None, max_length=100,
 null=True)
     desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Личность'
+        verbose_name_plural = 'Личности'
