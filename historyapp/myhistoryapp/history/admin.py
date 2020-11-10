a956d9f336c5d5782066a1243b4b8b8309492282
plagueevgeny <PlagueEvgeny@yandex.ru>
2020-11-10 14:15:48 +0500

    Creating two models

--- historyapp/myhistoryapp/history/admin.py
+++ historyapp/myhistoryapp/history/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from history.models import Category
+from history.models import Personality
 
 
 admin.site.register(Category)
+admin.site.register(Personality)
 # Register your models here.
