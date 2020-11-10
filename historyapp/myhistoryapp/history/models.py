2da62932c0f39dd5295877135d53a89cbc3b8bea
plagueevgeny <PlagueEvgeny@yandex.ru>
2020-11-10 14:14:15 +0500

    Creating two models

--- historyapp/myhistoryapp/history/models.py
+++ historyapp/myhistoryapp/history/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Personality(models.Model):
+    name = models.CharField(max_length=64)
+    photo = models.ImageField('Картинка', upload_to='static/img', height_field=None, width_field=None, max_length=100,
+null=True)
+    desc = models.TextField(blank=True)
